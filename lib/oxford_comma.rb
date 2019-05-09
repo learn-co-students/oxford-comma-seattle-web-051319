1e84fc6010fb3f579e23f51ddbc684b5883dcb02
bustling-maker-4835 <chopra.chet@gmail.com>
2019-05-09 18:58:31 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
-def oxford_comma(array)
-
+def oxford_comma(arr)
+  if arr.size() == 1
+    return arr[0]
+  end
+  if arr.size() == 2
+    return "#{arr[0]} and #{arr[1]}"
+  end
+  str = ""
+  amt = arr.size() - 1
+  amt.times do |i|
+    str += "#{arr[i]}, "
+  end
+  str += "and #{arr.last}"
+  return str
 end
+
